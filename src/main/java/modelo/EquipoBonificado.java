31f92777c421a050919961427c9de849ffbcf9d1
Rosa Maria Peña <rosamariapca10@gmail.com>
2026-04-05 21:24:06 -0500

    implementación de herencia con la clase EquipoBonificado y sobrescritura de calcularPuntos

--- src/main/java/modelo/EquipoBonificado.java
+++ src/main/java/modelo/EquipoBonificado.java
@@ -1,13 +1,24 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package modelo;
 
 /**
  *
  * @author rosam
  */
-public class EquipoBonificado {
+public class EquipoBonificado extends Equipo {
     
+    // Herencia
+    private int bono;
+
+    public EquipoBonificado(String nombre, int pg, int pe, int pp, int pj, int bono) {
+        super(nombre, pg, pe, pp, pj);
+        this.bono = bono;
+    }
+
+    // Polimorfismo
+    @Override
+    public int calcularPuntos() {
+        return super.calcularPuntos() + bono;
+    }
 }
+
