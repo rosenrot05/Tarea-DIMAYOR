72a2f96501fb36256a87042dcbde0b11a2d21895
Rosa Maria Peña <rosamariapca10@gmail.com>
2026-04-05 21:26:20 -0500

    creacion de LigaController con registro de equipos y almacenamiento en ArrayList

--- src/main/java/controlador/LigaController.java
+++ src/main/java/controlador/LigaController.java
@@ -1,13 +1,69 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package controlador;
 
 /**
  *
  * @author rosam
  */
+
+import modelo.Equipo;
+import java.util.ArrayList;
+
 public class LigaController {
-    
+        private ArrayList<Equipo> listaEquipos;
+
+    public LigaController() {
+        listaEquipos = new ArrayList<>();
+    }
+
+    // registrar equipos
+    public void registrar(Equipo e) {
+        listaEquipos.add(e);
+    }
+
+    // sobrecarga: buscar por nombre
+    public Equipo buscar(String nombre) {
+        for (Equipo e : listaEquipos) {
+            if (e.getNombre().equalsIgnoreCase(nombre)) {
+                return e;
+            }
+        }
+        return null;
+    }
+
+    // sobrecarga: buscar por índice
+    public Equipo buscar(int index) {
+        if (index >= 0 && index < listaEquipos.size()) {
+            return listaEquipos.get(index);
+        }
+        return null;
+    }
+
+    public ArrayList<Equipo> obtenerLista() {
+        return listaEquipos;
+    }
+
+    // mayor puntaje
+    public int mayorPuntaje() {
+        int mayor = 0;
+        for (Equipo e : listaEquipos) {
+            if (e.calcularPuntos() > mayor) {
+                mayor = e.calcularPuntos();
+            }
+        }
+        return mayor;
+    }
+
+    // cuantos equipos tienen el mayor puntaje
+    public int contarMayor() {
+        int mayor = mayorPuntaje();
+        int contador = 0;
+
+        for (Equipo e : listaEquipos) {
+            if (e.calcularPuntos() == mayor) {
+                contador++;
+            }
+        }
+        return contador;
+    }
 }
