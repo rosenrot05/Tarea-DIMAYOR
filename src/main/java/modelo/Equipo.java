91f7071b13bf52a51d663156a97a35341f4c5c10
Rosa Maria Peña <rosamariapca10@gmail.com>
2026-04-05 21:22:02 -0500

    Creación de la clase Equipo con atributos y método calcularPuntos (polimorfismo base)

--- src/main/java/modelo/Equipo.java
+++ src/main/java/modelo/Equipo.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package modelo;
 
 /**
@@ -9,5 +6,25 @@ package modelo;
  * @author rosam
  */
 public class Equipo {
-    
+    // atributos
+    protected String nombre;
+    protected int pg, pe, pp, pj;
+
+    public Equipo(String nombre, int pg, int pe, int pp, int pj) {
+        this.nombre = nombre;
+        this.pg = pg;
+        this.pe = pe;
+        this.pp = pp;
+        this.pj = pj;
+    }
+
+    // polimorfismo
+    public int calcularPuntos() {
+        return (pg * 3) + pe;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
 }
+    
